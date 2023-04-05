ea692afac5ad57de1511df6b678dc151b575c237
ettige <118074464+ettige@users.noreply.github.com>
2023-04-05 18:16:00 +0330

    Main

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
-window.addEventListener("gamepadconnected", (event) => {
-    console.log(navigator.getGamepads());
-  });
-  window.onload=navigator.getGamepads().mapping;
+ //start select
+ class Province{
+  id;
+  name;
+  citiesCount;
+  perName;
+  
+  
+  constructor(id,name,citiesCount=0){
+    this.id=id;
+    this.name=name;
+    this.citiesCount=citiesCount;
+  }
+}
+class City{
+  id;
+  provinceId;
+  name;
+  perName;
+  
+  constructor(id,provinceId,name,perName){
+    this.id=id;
+    this.provinceId=provinceId;
+    this.name=name;
+    this.perName;
+  }
+}
+const provinces=[new Province(1,"Gilan"),new Province(2,"Tehran")];
+const cities=[new City(1,1,"Masal","ماسال"),new City(2,1,"Shanderman","شاندرمن"),new City(3,2,"Tehran","تهران")];
+
+const provincesS=document.getElementById("provinces");
+const citiesS=document.getElementById("cities");
+provinces.forEach(element=>{
+  let option=document.createElement("option");
+  option.value=element.id;
+  option.textContent=element.name;
+  provincesS.appendChild(option);
+})
+provincesS.addEventListener("change",e=>{
+  let id=e.target.value;
+  let relatesCities=cities.filter(x=>x.provinceId.toString()===id);
+  citiesS.innerHTML="";
+  relatesCities.forEach(element=>{
+    let option=document.createElement("option");
+    option.value=element.id;
+    option.textContent=element.name;
+    citiesS.appendChild(option);
+  })     
+})
